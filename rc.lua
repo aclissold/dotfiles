fbf2a7e732ab29bdb99436abc28d4eb9d743f473
Andrew Clissold <ajclisso@oakland.edu>
2013-07-19 11:05:24 -0400

    rc.lua: enable tile-bottom layout for vertical columns

--- rc.lua
+++ rc.lua
@@ -112,9 +112,9 @@ modkey = "Mod4"
 layouts =
 {
     awful.layout.suit.tile,
+    awful.layout.suit.tile.bottom,
     awful.layout.suit.floating,
     -- awful.layout.suit.tile.left,
-    -- awful.layout.suit.tile.bottom,
     -- awful.layout.suit.tile.top,
     -- awful.layout.suit.fair,
     -- awful.layout.suit.fair.horizontal,
