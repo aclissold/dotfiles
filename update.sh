7dacb59b51fdf3ebf8bc62bfc8e270844e7b4ce8
Andrew Clissold <andrewclissold@gmail.com>
2014-04-30 19:52:04 -0400

    Rewrite update.sh

--- update.sh
+++ update.sh
@@ -5,33 +5,34 @@
 # files / symlinks already exist where these ones belong--ln -s fails if
 # something already exists in a given location under the same name.
 
-if [[ -n $(pwd | grep '/.dotfiles$') ]]
+symlink () {
+    for file in $@
+    do
+        # Skip symlinks that have already been made
+        if [[ ! -h $HOME/$file ]]
+        then
+            echo 'New symlink: ~/'"$file"
+            ln -s $(pwd)/$file $HOME
+        fi
+    done
+}
+
+if [[ -n $(pwd | grep "^$HOME/.dotfiles$") ]]
 then
-    echo 'Making symlinks... '
-    ln -s $(pwd)/.fonts.conf ~
-    ln -s $(pwd)/.gitconfig ~
-    ln -s $(pwd)/.gitignore ~
-    ln -s $(pwd)/.tmux.conf ~
-    ln -s $(pwd)/.pylintrc ~
-    ln -s $(pwd)/.pythonrc ~
-    ln -s $(pwd)/.vim ~
-    ln -s $(pwd)/.vimrc ~
-    ln -s $(pwd)/.xinitrc ~
-    ln -s $(pwd)/.zsh ~
-    ln -s $(pwd)/.zshrc ~
-    ln -s $(pwd)/.irbrc ~
-    ln -s $(pwd)/.gemrc ~
+    symlink .gitconfig .gitignore .tmux.conf .pylintrc .pythonrc .vim \
+        .vimrc .xinitrc .zsh .zshrc .irbrc .gemrc
+
     if [[ $(uname) == 'Darwin' ]]
     then
-        ln -s $(pwd)/.slate ~
-        ln -s $(pwd)/.vimrc ~/.xvimrc
+        symlink .slate
+    else
+        symlink .fonts.conf
     fi
+
     mkdir -p ~/.vimundo
     mkdir -p ~/.Trash
-    echo 'Done.'
-    echo 'Updating submodules... '
+
     git submodule update --init
-    echo 'Done.'
 else
     echo "Command failed. Please ensure that
     1) this repository exists at ~/.dotfiles, and
